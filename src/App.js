fde67d4e0ccc961badef31d65e4e71283172eea0
Pedro Werkhaizer <pedrowerkhaizer@users.noreply.github.com>
2020-03-31 16:02:19 -0300

    Added design features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './global.css'
+
 
 function App() {
+  const [ convertType, setConvertType ] = useState('moeda');
+  const [ typeOne, setTypeOne ] = useState('usd');
+  const [ typeTwo, setTypeTwo ] = useState('brl');
+
+  function handleConvertion(conversionType, firstValue, secondValue){
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="convertContainer">
+      <div className="titleContainer">
+        <p className="fixedTitle">Converter </p>
+        <a className="variableTitle" href="/">{convertType}</a>
+        <select name="convertTypeSelector" className="selector" 
+        onChange={e => setConvertType(e.target.options[e.target.selectedIndex].text.toLocaleLowerCase())}>
+          <option value="moeda">Moeda</option>
+          <option value="massa">Massa</option>
+          <option value="comprimento">Comprimento</option>
+      </select>
+      </div>
+      
+      <div className="inputContainer">
+        <input type="text" placeholder="1"/>
+        <p className="inputType">usd</p>
+      </div>
+      <div className="parameterSelector">
+      <a href="/">{typeOne}</a>
+      <select name="typeOneSelector" className="selector"
+      onChange={e => setTypeOne(e.target.options[e.target.selectedIndex].text.toLocaleLowerCase())}>
+        <option value="usd">USD</option>
+        <option value="brl">REAL</option>
+        <option value="peso">PESO ARG</option>
+      </select>
+      </div>
+
+      <p className="equalsTitle">é igual a:</p>
+
+      <div className="inputContainer">
+        <input type="text" placeholder="5.17"/>
+        <p className="inputType">real</p>
+      </div>
+      <div className="parameterSelector">
+      <a href="/">{typeTwo}</a> 
+      <select name="typeTwoSelector" className="selector"
+      onChange={e => setTypeTwo(e.target.options[e.target.selectedIndex].text.toLocaleLowerCase())}>
+        <option value="brl">REAL</option>
+        <option value="usd">USD</option>
+        <option value="peso">PESO ARG</option>
+      </select>
+      </div>
+
     </div>
   );
 }
